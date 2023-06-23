ebd53fddd2b6c70711e4a4428392ae28faadcd52
chintansheth2001 <chintansheth2001@gmail.com>
2023-06-24 00:25:32 +0530

    Change heder line

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 import "./App.scss";
+import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import TitleRow from "./components/TitleRow";
 import FilterRow from "./components/FilterRow";
 import TaskRow from "./components/TaskRow";
-import Header from "./components/Header";
 import { AppContext } from "./components/AppContext";
 import { TOGGLE_SIDEBAR } from "./components/ActionType";
 
